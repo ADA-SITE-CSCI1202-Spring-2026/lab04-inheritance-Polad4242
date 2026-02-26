f4b541ab5152210410f77a01be724eff7b277ca8
Polad4242 <poladshi@code.edu.az>
2026-02-26 10:43:36 +0400

    Add files via upload
    8a

---
+++ Person.java
@@ -0,0 +1,62 @@
+public class Person {
+
+    private String firstName;
+    private String lastName;
+    private String gender;
+
+    // Constructor
+    public Person(String firstName, String lastName, String gender) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+    }
+
+    // Default constructor (optional but good practice)
+    public Person() {
+    }
+
+    // Getters
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    // Setters
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    // toString method
+    @Override
+    public String toString() {
+        return "Person[firstName=" + firstName +
+               ", lastName=" + lastName +
+               ", gender=" + gender + "]";
+    }
+
+    // equals method
+    public boolean equals(Person p) {
+        if (p == null) {
+            return false;
+        }
+
+        return this.firstName.equals(p.firstName) &&
+               this.lastName.equals(p.lastName) &&
+               this.gender.equals(p.gender);
+    }
+}
